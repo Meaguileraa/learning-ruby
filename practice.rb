ce438423b2cf02daf08f06fc966461a2ec98b564
Marissa Aguilera <marissaguilera@yahoo.com>
2021-03-26 17:43:40 -0700

    Puts, Prints, & P

---
+++ practice.rb
@@ -0,0 +1,11 @@
+
+
+#_____________________________________________
+#Ways to print (p, puts, print)
+
+p "Marissa is learning Ruby" #returns as a string/ good for debugging 
+print "Marissa is learning Ruby" 
+puts "Marissa is learning Ruby"
+# Puts adds a new line at the end of a message and print does not. 
+
+#_____________________________________________
