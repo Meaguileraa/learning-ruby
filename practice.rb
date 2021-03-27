c715c6dfd98dbbcdbc21555faad8484684ab1535
Marissa Aguilera <marissaguilera@yahoo.com>
2021-03-26 17:44:13 -0700

    Strings

--- practice.rb
+++ practice.rb
@@ -9,3 +9,14 @@ puts "Marissa is learning Ruby"
 # Puts adds a new line at the end of a message and print does not. 
 
 #_____________________________________________
+
+#What to do with strings 
+puts "bacon".size #length
+puts "bacon".upcase #all uppercase 
+puts "bacon".gsub("acon", "inary")
+puts "Great".gsub("eat", "oss") # substitutes characters 
+puts "bacon".chars #returns each character of the string 
+
+#_____________________________________________
+
+puts "1" + "1" #11
